2f9ab234807ddc858848de0a97848405c91b46ef
dolee <dollosa.christian@gmail.com>
2021-09-18 08:17:04 +0800

    asda

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,8 +68,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => Rails.application.credentials.gmail_mailer[:username],
-    :password             => Rails.application.credentials.gmail_mailer[:password],
+    :user_name            => 'emporia.wallet@gmail.com',
+    :password             => 'ealguzuipzvijcur',
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
