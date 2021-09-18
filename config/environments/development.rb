851312f36aebc002ffbeaa5cafeb83bf0a6379c6
dolee <dollosa.christian@gmail.com>
2021-09-18 09:19:03 +0800

    test git action

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,8 +69,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => Rails.application.credentials.gmail_mailer[:username],
-    :password             => Rails.application.credentials.gmail_mailer[:password],
+    :user_name            => Rails.application.credentials.config[:gmail_mailer][:username],
+    :password             => Rails.application.credentials.config[:gmail_mailer][:password],
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
