25179a79017a17d97f9c4f05749f26b72652456f
mumble-07 <andreapauline.deguzman@gmail.com>
2021-09-22 20:33:29 +0800

    connection of users, portfolios, and markets

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,7 +1,35 @@
 class PortfoliosController < ApplicationController
+  before_action :authenticate_user!
+  #before_action :portfolio
   def new
-    @users = User.all
     @markets = Market.all
+    @markets = Market.find(params[:market_id])
+    @portfolio = Portfolio.new
     @portfolios = Portfolio.all
+    @portfolios = Portfolio.find_by_id(params[:user_id])
+    @portfolios = Portfolio.find_by_id(params[:market_id])
   end
+
+  def create
+    @portfolio = @markets.portfolios.build(portfolio_params)
+    if @portfolio.save
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:user_id, :market_symbol, :hist_price, :amount, :market_id)
+  end
+
+  # def portfolio
+  #   @portfolio = Portfolio.find(params[:user_id])
+  # end
+
+  # def user
+  #   @user = User.find(params[:user_id])
+  # end
 end
