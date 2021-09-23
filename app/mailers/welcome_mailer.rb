9d15ff2c8785d0c4ffb9a2187390b1c47b86bf8a
dolee <dollosa.christian@gmail.com>
2021-09-23 22:56:58 +0800

    created a separate welcome email for admin created user accounts

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -2,4 +2,8 @@ class WelcomeMailer < ApplicationMailer
   def welome_account_email(email)
     mail(to: email, subject: 'Welome to Emporia Wallet!')
   end
+
+  def welome_account_email_admin(email)
+    mail(to: email, subject: 'Welome to Emporia Wallet!')
+  end
 end
