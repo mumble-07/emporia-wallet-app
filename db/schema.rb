291da825fe4e413644ef7e4c0c1e378613651bfc
mumble-07 <andreapauline.deguzman@gmail.com>
2021-09-23 21:22:33 +0800

    added column of transaction type to portfolios table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_22_123923) do
+ActiveRecord::Schema.define(version: 2021_09_23_130956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2021_09_22_123923) do
     t.integer "market_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "transaction_type"
   end
 
   create_table "users", force: :cascade do |t|
