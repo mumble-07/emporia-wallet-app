002ca814a620ab2c774a855424ec1f3231802d4b
mumble-07 <andreapauline.deguzman@gmail.com>
2021-09-24 00:45:16 +0800

    modified user helper, commented first some methods, need to finish sell functionality first

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -17,12 +17,7 @@ module UsersHelper
   end
 
   def percent_lg(curr_price, invested)
-    invested ||= 0
-    usd_to_php = 50.06
-    convert_price = curr_price * usd_to_php
-    with_interest_total = convert_price * 1.05
-    percent_value = ((with_interest_total - invested) / with_interest_total) * 100
-    number_to_percentage(percent_value, precision: 3)
+    # will finish sell first
   end
 
   def current_value(curr_price)
@@ -37,11 +32,6 @@ module UsersHelper
   end
 
   def get_val_invest_diff(invest, curr_price)
-    invest ||= 0
-    usd_to_php = 50.06
-    convert_price = curr_price * usd_to_php
-    with_interest_total = convert_price * 1.05
-    profit_loss = with_interest_total - invest
-    number_to_currency(profit_loss, unit: 'PHP ')
+    # will finish sell first
   end
 end
