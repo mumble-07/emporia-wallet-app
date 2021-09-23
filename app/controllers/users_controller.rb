e8d701929205d498228a6a01001fa3b3ddd08d4c
mumble-07 <andreapauline.deguzman@gmail.com>
2021-09-24 00:20:14 +0800

    users controller methods for buy and sell

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @markets = Market.all
-    @portfolios = Portfolio.all
+    @portfolios = current_user.portfolios
     @wallets = Wallet.all
   end
 end
