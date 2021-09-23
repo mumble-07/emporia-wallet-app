e05e3e7df7577f42a28c9b3fa6834cbe745443f8
mumble-07 <andreapauline.deguzman@gmail.com>
2021-09-24 00:18:35 +0800

    rsec models testing

--- spec/models/portfolio_spec.rb
+++ spec/models/portfolio_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Portfolio, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'when user buy or sell stocks' do
+    it 'is not allowed without user_id' do
+      portfolio = described_class.new(user_id: nil, market_symbol: 'AA', hist_price: 99.99, amount: 100.50)
+      expect(portfolio).not_to be_valid
+    end
+
+    it 'is not allowed without market symbol' do
+      portfolio = described_class.new(user_id: 1, market_symbol: nil, hist_price: 99.99, amount: 100.50)
+      expect(portfolio).not_to be_valid
+    end
+
+    it 'is not allowed without sotck price (hist_price)' do
+      portfolio = described_class.new(user_id: 1, market_symbol: 'AA', hist_price: nil, amount: 100.50)
+      expect(portfolio).not_to be_valid
+    end
+  end
 end
