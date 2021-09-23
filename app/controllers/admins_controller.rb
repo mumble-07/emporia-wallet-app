5dc9832477bd5c2667f27ebae0512d0c59547945
dolee <dollosa.christian@gmail.com>
2021-09-23 23:35:24 +0800

    removed welcome mailer in admins_controller upon user creation

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -33,7 +33,6 @@ class AdminsController < ApplicationController
     @trader.approved = true
     @trader.save
     if @trader.save
-      WelcomeMailer.welome_account_email(@trader.email).deliver_now
       redirect_back fallback_location: admins_add_user_path, success: 'Successfully created a trader'
     else
       redirect_back fallback_location: admins_add_user_path, danger: 'Kindly double check all information before submitting'
