ae8cee76207a4205d89a9e18e4e840675a6807e5
mumble-07 <andreapauline.deguzman@gmail.com>
2021-09-24 15:12:18 +0800

    user portfolio test and open transaction history, make buy/sell

--- spec/requests/portfolios_spec.rb
+++ spec/requests/portfolios_spec.rb
@@ -1,7 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe 'Portfolios', type: :request do
+  let(:user) { create(:user) }
+  let(:user_pending) { create(:user_pending) }
+  let(:market) { create(:market) }
+  let(:buy_market) { create(:buy_market) }
+  let(:sell_market) { create(:sell_market) }
+
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'returns the index page' do
+      get root_path
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe 'User gets to open their portfolio' do
+    before do
+      sign_in user
+    end
+
+    it 'open user portfolio' do
+      get users_path
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'open user transaction history' do
+      get transactions_logs_path
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'allow user to make a buy/sell' do
+      market = create(:market)
+      get new_portfolio_path(market_id: market.id)
+      expect(response).to have_http_status(:ok)
+    end
   end
 end
