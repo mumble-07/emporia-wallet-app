eed88626ea3338330364bd7972fc7b6aeaafef25
dolee <dollosa.christian@gmail.com>
2021-09-24 22:26:53 +0800

    fixed precision of unit data in transaction logs

--- app/helpers/transactions_logs_helper.rb
+++ app/helpers/transactions_logs_helper.rb
@@ -2,9 +2,9 @@ module TransactionsLogsHelper
   def transaction_logs_parser(transaction)
     case transaction.transaction_type
     when 'BUY'
-      "Bought #{transaction.units.round(2)} units of #{transaction.market_symbol} stocks at Php #{transaction.hist_stock_price.round(2)} per stock for a total of Php #{transaction.amount.round(2)}. "
+      "Bought #{transaction.units.round(4)} units of #{transaction.market_symbol} stocks at Php #{transaction.hist_stock_price.round(7)} per stock for a total of Php #{transaction.amount.round(2)}. "
     when 'SELL'
-      "Sold #{transaction.units.round(2)} units of #{transaction.market_symbol} stocks at Php #{transaction.hist_stock_price.round(2)} per stock for a total of Php #{transaction.amount.round(2)}. "
+      "Sold #{transaction.units} units of #{transaction.market_symbol} stocks at Php #{transaction.hist_stock_price.round(7)} per stock for a total of Php #{transaction.amount.round(2)}. "
     end
   end
 end
