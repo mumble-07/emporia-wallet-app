a98c7c54e7fa329f28ad5d053c9c1d55930f8b2d
mumble-07 <andreapauline.deguzman@gmail.com>
2021-09-24 15:10:39 +0800

    admin login sign out test

--- spec/requests/admins_spec.rb
+++ spec/requests/admins_spec.rb
@@ -1,7 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe 'Admins', type: :request do
+  let(:admin) { create(:admin) }
+
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'returns the index page' do
+      get root_path
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  it 'signs admin in' do
+    sign_in admin
+    get root_path
+    expect(response).to have_http_status(:ok)
+  end
+
+  it 'signs admin out' do
+    sign_out admin
+    get root_path
+    expect(response).to have_http_status(:ok)
+  end
+
+  it 'lets admin open admin settings' do
+    sign_in admin
+    get admins_trader_settings_path
+    expect(response).to have_http_status(:ok)
+  end
+
+  it 'lets admin approve pending users' do
+    sign_in admin
+    get admins_approvals_path
+    expect(response).to have_http_status(:ok)
   end
 end
