4762a441cf0062a3a43833b175d3f400d7a1d973
mumble-07 <andreapauline.deguzman@gmail.com>
2021-09-24 15:13:09 +0800

    additional user spec test

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,10 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
+  let(:user) { create(:user) }
+  let(:user_pending) { create(:user_pending) }
+
+  describe 'User sign in and log out' do
     it 'returns the index page' do
       get root_path
       expect(response).to have_http_status(:ok)
     end
+  end
+
+  it 'signs user in' do
+    sign_in user
+    get markets_path
+    expect(response).to have_http_status(:ok)
+  end
+
+  it 'signs user out' do
+    sign_out user
+    get root_path
+    expect(response).to have_http_status(:ok)
+  end
+
+  it 'says user approval is still pending' do
+    sign_in user_pending
+    get markets_path
+    expect(response).not_to render_template(:index)
   end
 end
